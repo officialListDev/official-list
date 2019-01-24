f388f7dd7baac316b652156f9e8811791e238f81
Evan Ruby <evanruby94@gmail.com>
2019-01-24 02:06:12 -0500

    Added eslint, removed unused files

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "react/jsx-filename-extension": 0,
+        "react/no-unused-prop-types": 0,
+    },
+    "globals": {
+        "document": true,
+    }
+}
