1b63d5becc8d25b412e7ff40f19c56404ea807c4
adamgoren <adamjgoren@gmail.com>
2019-11-25 15:48:23 -0500

    Add server routing

--- server/server.js
+++ server/server.js
@@ -7,14 +7,22 @@ const next = require('next');
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
+
+const apiRouter = require('./routes/api.js');
     
 app.prepare()
   .then(() => {
     const server = express();
     
+    // Server endpoint
+    // Add middleware here
+    // https://dev.to/aurelkurtula/introduction-to-nextjs---adding-express-and-mongo-to-the-project-2d38
+    server.use('/api', apiRouter);
+
+    // Default Path
     server.get('*', (req, res) => {
       return handle(req, res);
-    });
+    });  
     
     server.listen(3000, (err) => {
       if (err) throw err;
