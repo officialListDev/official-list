9a91fdd676143418b21f4c9d2f3a623303296317
adamgoren <adamjgoren@gmail.com>
2019-11-26 16:50:45 -0500

    use template literals for query string formatting

--- server/model/queries.js
+++ server/model/queries.js
@@ -4,7 +4,17 @@ module.exports = {
   createWatchlistsTable: 'CREATE TABLE IF NOT EXISTS watchlists (id SERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL, description VARCHAR)',
   createAuditionsTable: 'CREATE TABLE IF NOT EXISTS auditions (id SERIAL PRIMARY KEY, actor_id INTEGER NOT NULL REFERENCES actors(id) ON DELETE CASCADE, date VARCHAR NOT NULL, show_name VARCHAR NOT NULL)',
   createNotesTable: 'CREATE TABLE IF NOT EXISTS notes (id SERIAL PRIMARY KEY, director_id INTEGER NOT NULL REFERENCES directors(id) ON DELETE CASCADE, actor_id INTEGER NOT NULL REFERENCES actors(id) ON DELETE CASCADE, text VARCHAR, private_flag BOOLEAN)',
-  createDirectorWatchlistsTable: 'CREATE TABLE IF NOT EXISTS director_watchlists (director_id INTEGER NOT NULL, watchlist_id INTEGER NOT NULL, PRIMARY KEY (director_id, watchlist_id), CONSTRAINT director_watchlists_director_id_fkey FOREIGN KEY (director_id) REFERENCES directors (id)MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE, CONSTRAINT director_watchlists_list_id FOREIGN KEY (watchlist_id) REFERENCES watchlists (id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE)',
+  createDirectorWatchlistsTable: `
+  CREATE TABLE IF NOT EXISTS director_watchlists 
+  (director_id INTEGER NOT NULL, 
+  watchlist_id INTEGER NOT NULL, 
+  PRIMARY KEY (director_id, watchlist_id), 
+    CONSTRAINT director_watchlists_director_id_fkey FOREIGN KEY (director_id) 
+      REFERENCES directors (id)
+      MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE, 
+    CONSTRAINT director_watchlists_list_id FOREIGN KEY (watchlist_id) 
+      REFERENCES watchlists (id) 
+      MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE)`,
   // https://stackoverflow.com/questions/35338711/cannot-drop-table-users-because-other-objects-depend-on-it
   dropDirectorsTable: 'DROP TABLE IF EXISTS directors cascade',
   dropActorsTable: 'DROP TABLE IF EXISTS actors cascade',
@@ -19,3 +29,23 @@ module.exports = {
   //   console.log('\n*********** visitsController.createVisit ****************', `\nMETHOD: ${req.method} \nENDPOINT: '${req.url}' \nBODY: ${JSON.stringify(req.body)} \nLOCALS: ${JSON.stringify(res.locals)} `);
   // },
 };
+
+/* Query to test that our director_watchlists JOIN table was created correctly
+SELECT                 
+    tc.table_schema, 
+    tc.constraint_name, 
+    tc.table_name, 
+    kcu.column_name, 
+    ccu.table_schema AS foreign_table_schema,
+    ccu.table_name AS foreign_table_name,
+    ccu.column_name AS foreign_column_name 
+FROM 
+    information_schema.table_constraints AS tc 
+    JOIN information_schema.key_column_usage AS kcu
+      ON tc.constraint_name = kcu.constraint_name
+      AND tc.table_schema = kcu.table_schema
+    JOIN information_schema.constraint_column_usage AS ccu
+      ON ccu.constraint_name = tc.constraint_name
+      AND ccu.table_schema = tc.table_schema
+WHERE tc.constraint_type = 'FOREIGN KEY' AND tc.table_name='director_watchlists';
+*/
