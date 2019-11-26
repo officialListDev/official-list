8acb588beecd3f8895a83136ba44bd4aeadaaced
adamgoren <adamjgoren@gmail.com>
2019-11-26 10:48:58 -0500

    correct drop table synax

--- server/model/queries.js
+++ server/model/queries.js
@@ -2,10 +2,11 @@ module.exports = {
   createDirectorsTable: 'CREATE TABLE IF NOT EXISTS directors (id SERIAL PRIMARY KEY, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, email VARCHAR NOT NULL, password VARCHAR NOT NULL, phone_number VARCHAR(15), company_name VARCHAR(50))',
   createActorsTable: 'CREATE TABLE IF NOT EXISTS actors (id SERIAL PRIMARY KEY, headshot VARCHAR, resume VARCHAR, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, email VARCHAR NOT NULL, password VARCHAR NOT NULL, phone_number VARCHAR(50), city VARCHAR(50) NOT NULL, facebook VARCHAR(50), twitter VARCHAR(50), instagram VARCHAR(50), youtube VARCHAR(50), bio VARCHAR)',
   createWatchlistsTable: 'CREATE TABLE IF NOT EXISTS watchlists (id SERIAL PRIMARY KEY, list_name VARCHAR(50) NOT NULL, list_description VARCHAR(300))',
-  createAuditionsTable: 'CREATE TABLE IF NOT EXISTS auditions (id SERIAL PRIMARY KEY, actor_id INTEGER NOT NULL REFERENCES actors(id), date VARCHAR NOT NULL, show_name VARCHAR NOT NULL)',
-  createNotesTable: 'CREATE TABLE IF NOT EXISTS notes (id SERIAL PRIMARY KEY, director_id INTEGER NOT NULL REFERENCES directors(id), actor_id INTEGER NOT NULL REFERENCES actors(id), text VARCHAR, private_flag BOOLEAN)',
+  createAuditionsTable: 'CREATE TABLE IF NOT EXISTS auditions (id SERIAL PRIMARY KEY, actor_id INTEGER NOT NULL REFERENCES actors(id) ON DELETE CASCADE, date VARCHAR NOT NULL, show_name VARCHAR NOT NULL)',
+  createNotesTable: 'CREATE TABLE IF NOT EXISTS notes (id SERIAL PRIMARY KEY, director_id INTEGER NOT NULL REFERENCES directors(id) ON DELETE CASCADE, actor_id INTEGER NOT NULL REFERENCES actors(id) ON DELETE CASCADE, text VARCHAR, private_flag BOOLEAN)',
   // createDirectorWatchlists: 'CREATE TABLE IF NOT EXISTS director_watchlists ()'
-  dropTables: 'DROP TABLE directors',
+  // https://stackoverflow.com/questions/35338711/cannot-drop-table-users-because-other-objects-depend-on-it
+  dropTables: 'DROP TABLE if exists directors cascade',
   // createSearchTable: 'CREATE TABLE IF NOT EXISTS searches (_id SERIAL PRIMARY KEY, url VARCHAR, result VARCHAR)',
   // postSearchTable: 'INSERT INTO searches (url, result) VALUES($1, $2)',
   // queryLogging: function (){
