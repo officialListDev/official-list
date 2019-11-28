a87a83ae24f7dafaea74a0e2dcf2321c691f6a31
RajeebTheGreat <rajeebthegreat@gmail.com>
2019-11-28 01:57:09 -0500

    Utilize express for routing api endpoints

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/hello', (req, res, next) => {
-  res.send({'hello': true});
+  res.send("Hello");
   return next();
 });
 
--- server/server.js
+++ server/server.js
@@ -2,35 +2,34 @@
  * https://blog.logrocket.com/how-to-build-a-server-rendered-react-app-with-next-express-d5a389e7ab2f/
  */
 const express = require('express');
+const server = express();
+
 const next = require('next');
-    
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const nextApp = next({ dev });
+
+const handle = nextApp.getRequestHandler();
+const bodyParser = require('body-parser');
 
 const apiRouter = require('./routes/api.js');
-const authRouter = require('./routes/auth.js');
+// const authRouter = require('./routes/auth.js');
     
-app.prepare()
+nextApp.prepare()
   .then(() => {
-    const server = express();
-    
-    // Server endpoint
-    // Add middleware here
-    // https://dev.to/aurelkurtula/introduction-to-nextjs---adding-express-and-mongo-to-the-project-2d38
+
+    server.use(bodyParser.json());
+    server.use(bodyParser.urlencoded({ extended: true }));
+
     server.use('/api', apiRouter);
 
-    // Handle all /auth endpoints, that is signup: /auth/signup, login: /auth/login
-    server.use('/auth', authRouter);
+    server.get('*', (req,res) => {
+      // console.log("Hello");
+      return handle(req,res) // for all the react stuff
+    })
 
-    // Default Path
-    server.get('*', (req, res) => {
-      return handle(req, res);
-    });  
-  
-    server.listen(3000, (err) => {
+    server.listen(3000, err => {
       if (err) throw err;
-      console.log('> Ready on http://localhost:3000');
+      console.log(`ready at http://localhost:${3000}`)
     });
   })
   .catch((ex) => {
