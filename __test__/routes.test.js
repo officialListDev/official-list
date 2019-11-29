7a7f1b0799502d3d7c98ae8788d0ea068fd85eda
RajeebTheGreat <rajeebthegreat@gmail.com>
2019-11-29 01:25:01 -0500

    Add tests for director signup and login endpoints

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -10,4 +10,41 @@ describe('Route integration', () => {
         .expect(200);
     });
   });
+
+  describe('Director signup', () => {
+    it('responds with newly created director name', async (done) => {
+      const data = [
+        {
+          'first_name': 'John',
+          'last_name': 'Doe',
+          'email': 'john@doe.com',
+          'password': 'supersecretpassword',
+          'phone_number': '8835732379',
+          'company_name': 'Smith Inc.',
+        },
+      ];
+      const result = await request(server)
+        .post('/auth/director/signup')
+        .send(data[0])
+        .expect('Content-Type', /application\/json/)
+        .expect(200);
+      expect(result.body.first_name).toEqual(data[0].first_name);
+      done();
+    });
+
+    it('responds with 200 status for login', async (done) => {
+      const data = [
+        {
+          'email': 'john@doe.com',
+          'password': 'supersecretpassword',
+        },
+      ];
+      const result = await request(server)
+        .post('/auth/director/login')
+        .send(data[0])
+        .expect('Content-Type', /application\/json/)
+        .expect(200);
+      done();
+    });
+  });
 });
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -10,7 +10,7 @@ const SALT_WORK_FACTOR = 10;
 authController.directorSignup = async (req, res, next) => {
 
   const errors = validationResult(req);
-    
+
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
   }
@@ -28,7 +28,7 @@ authController.directorSignup = async (req, res, next) => {
               return next;
             })
             .catch(err => console.log('Error:', err))
-            .finally( client.release() );
+            .finally(client.release());
         })
         .catch(err => console.log('error connecting', err));
     });
@@ -37,7 +37,7 @@ authController.directorSignup = async (req, res, next) => {
 authController.directorLogin = async (req, res, next) => {
 
   const errors = validationResult(req);
-    
+  console.log(errors.array());
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
   }
@@ -45,23 +45,23 @@ authController.directorLogin = async (req, res, next) => {
   const { email, password } = req.body;
 
   const values = [email];
-  
+
   await pool.connect()
     .then((client) => {
       client.query(queries.getDirector, values, (err, director) => {
-        if(err){
+        if (err) {
           return res.status(404).json({ errors: 'Incorrect email# or password.' });
         }
         // console.log('user\'s password', password, director.rows[0].password);
         // If email address is found on the database
-        if(director.rows[0]){
+        if (director.rows[0]) {
           bcrypt.compare(password, director.rows[0].password, (err, passwordMatch) => {
             client.release();
             // console.log('what is err', err, passwordMatch);
-            if(err || !passwordMatch){
+            if (err || !passwordMatch) {
               return res.status(404).json({ errors: 'Incorrect email or password#.' });
             }
-            return res.send('Logged in');
+            return res.status(200).json({ message: 'User created' });
           });
         }
       });
@@ -73,7 +73,7 @@ authController.directorLogin = async (req, res, next) => {
 authController.actorSignup = async (req, res, next) => {
 
   const errors = validationResult(req);
-    
+
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
   }
@@ -82,7 +82,7 @@ authController.actorSignup = async (req, res, next) => {
 
   bcrypt.hash(password, SALT_WORK_FACTOR)
     .then(async (hashedPassword) => {
-      const values = [ headshot, resume, first_name, last_name, email, hashedPassword, phone_number, city, facebook, twitter, instagram, youtube, bio ];
+      const values = [headshot, resume, first_name, last_name, email, hashedPassword, phone_number, city, facebook, twitter, instagram, youtube, bio];
       await pool.connect()
         .then((client) => {
           return client.query(queries.seedActorsTable, values)
@@ -90,7 +90,7 @@ authController.actorSignup = async (req, res, next) => {
               return res.status(200).json(response.rows[0]);
             })
             .catch(err => console.log('Error:', err))
-            .finally( client.release() );
+            .finally(client.release());
         })
         .catch(err => console.log('error connecting', err));
     });
@@ -99,27 +99,27 @@ authController.actorSignup = async (req, res, next) => {
 authController.actorLogin = async (req, res, next) => {
 
   const errors = validationResult(req);
-    
+
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
   }
 
   const { email, password } = req.body;
   const values = [email];
-  
+
   await pool.connect()
     .then((client) => {
       client.query(queries.getActor, values, (err, actor) => {
-        if(err){
+        if (err) {
           return res.status(404).json({ errors: 'Incorrect email# or password.' });
         }
         // console.log('user\'s password', password, actor.rows[0].password);
         // If email address is found on the database
-        if(actor.rows[0]){
+        if (actor.rows[0]) {
           bcrypt.compare(password, actor.rows[0].password, (err, passwordMatch) => {
             client.release();
             // console.log('what is err', err, passwordMatch);
-            if(err || !passwordMatch){
+            if (err || !passwordMatch) {
               return res.status(404).json({ errors: 'Incorrect email or password#.' });
             }
             return res.send('Logged in');
@@ -133,46 +133,46 @@ authController.actorLogin = async (req, res, next) => {
 // first_name, last_name, email, password, phone_number, company_name
 authController.validate = (method) => {
   switch (method) {
-  case 'directorSignup': {
-    return [
-      body('first_name').isLength({ min: 1 }).withMessage('First Name is Required').trim().escape(),
-      body('last_name').isLength({ min: 1 }).withMessage('Last Name is Required').trim().escape(),
-      body('email').isEmail().normalizeEmail().withMessage('Enter valid email'),
-      body('password').isLength({ min: 6 }).withMessage('Password should be minimum 6 characters'),
-      body('phone_number').isMobilePhone().withMessage('Enter valid phone number'),
-      body('company_name').exists(),
-    ];
-  }
-  case 'directorLogin': {
-    return [
-      body('email').isEmail().normalizeEmail().withMessage('Enter valid email'),
-      body('password').isLength({ min: 6 }).withMessage('Password should be minimum 6 characters'),
-    ];
-  }
-  // headshot, resume, first_name, last_name, email, password, phone_number, city, facebook, twitter, instagram, youtube, bio
-  case 'actorSignup': {
-    return [
-      body('headshot'),
-      body('resume').exists(),
-      body('first_name').isLength({ min: 1 }).withMessage('First Name is Required').trim().escape(),
-      body('last_name').isLength({ min: 1 }).withMessage('Last Name is Required').trim().escape(),
-      body('email').isEmail().normalizeEmail().withMessage('Enter valid email'),
-      body('password').isLength({ min: 6 }).withMessage('Password should be minimum 6 characters'),
-      body('phone_number').isMobilePhone().withMessage('Enter valid phone number'),
-      body('city').isLength({ min: 1 }).withMessage('City is Required').trim().escape(),
-      body('facebook'),
-      body('twitter'),
-      body('instagram'),
-      body('youtube'),
-      body('bio').isLength({ min: 1 }).withMessage('Bio is required').trim().escape(),
-    ];
-  }
-  case 'actorLogin': {
-    return [
-      body('email').isEmail().normalizeEmail().withMessage('Enter valid email'),
-      body('password').isLength({ min: 6 }).withMessage('Password should be minimum 6 characters'),
-    ];
-  }
+    case 'directorSignup': {
+      return [
+        body('first_name').isLength({ min: 1 }).withMessage('First Name is Required').trim().escape(),
+        body('last_name').isLength({ min: 1 }).withMessage('Last Name is Required').trim().escape(),
+        body('email').isEmail().normalizeEmail().withMessage('Enter valid email'),
+        body('password').isLength({ min: 6 }).withMessage('Password should be minimum 6 characters'),
+        body('phone_number').isMobilePhone().withMessage('Enter valid phone number'),
+        body('company_name').exists(),
+      ];
+    }
+    case 'directorLogin': {
+      return [
+        body('email').isEmail().normalizeEmail().withMessage('Enter valid email'),
+        body('password').isLength({ min: 6 }).withMessage('Password should be minimum 6 characters'),
+      ];
+    }
+    // headshot, resume, first_name, last_name, email, password, phone_number, city, facebook, twitter, instagram, youtube, bio
+    case 'actorSignup': {
+      return [
+        body('headshot'),
+        body('resume').exists(),
+        body('first_name').isLength({ min: 1 }).withMessage('First Name is Required').trim().escape(),
+        body('last_name').isLength({ min: 1 }).withMessage('Last Name is Required').trim().escape(),
+        body('email').isEmail().normalizeEmail().withMessage('Enter valid email'),
+        body('password').isLength({ min: 6 }).withMessage('Password should be minimum 6 characters'),
+        body('phone_number').isMobilePhone().withMessage('Enter valid phone number'),
+        body('city').isLength({ min: 1 }).withMessage('City is Required').trim().escape(),
+        body('facebook'),
+        body('twitter'),
+        body('instagram'),
+        body('youtube'),
+        body('bio').isLength({ min: 1 }).withMessage('Bio is required').trim().escape(),
+      ];
+    }
+    case 'actorLogin': {
+      return [
+        body('email').isEmail().normalizeEmail().withMessage('Enter valid email'),
+        body('password').isLength({ min: 6 }).withMessage('Password should be minimum 6 characters'),
+      ];
+    }
   }
 };
 
