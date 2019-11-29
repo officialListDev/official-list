8bc52eb1933bd9f243b9cac95687ae3ba4d748d0
RajeebTheGreat <rajeebthegreat@gmail.com>
2019-11-29 00:39:54 -0500

    Setup jest testing

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -1,5 +1,13 @@
-describe('Sample Test', () => {
-  it('should test that true === true', () => {
-    expect(true).toBe(true)
-  })
-})
+const request = require('supertest');
+const server = 'http://localhost:3000';
+
+describe('Route integration', () => {
+  describe('/', () => {
+    it('responds with 200 status and text/html content type', () => {
+      return request(server)
+        .get('/')
+        .expect('Content-Type', /text\/html/)
+        .expect(200);
+    });
+  });
+});
