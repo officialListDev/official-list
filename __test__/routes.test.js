7a23021cece7301f342abc9e58b1d4cbcef8d2cc
RajeebTheGreat <rajeebthegreat@gmail.com>
2019-11-29 01:33:33 -0500

    More tests

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const server = 'http://localhost:3000';
 
 describe('Route integration', () => {
-  describe('/', () => {
+  describe('Homepage Test', () => {
     it('responds with 200 status and text/html content type', () => {
       return request(server)
         .get('/')
@@ -12,7 +12,7 @@ describe('Route integration', () => {
   });
 
   describe('Director signup', () => {
-    it('responds with newly created director name', async (done) => {
+    it('responds with 200 status and application/json content type', async (done) => {
       const data = [
         {
           'first_name': 'John',
@@ -28,11 +28,12 @@ describe('Route integration', () => {
         .send(data[0])
         .expect('Content-Type', /application\/json/)
         .expect(200);
-      expect(result.body.first_name).toEqual(data[0].first_name);
       done();
     });
+  });
 
-    it('responds with 200 status for login', async (done) => {
+  describe('Director Login', () => {
+    it('responds with 200 status and application/json content type', async (done) => {
       const data = [
         {
           'email': 'john@doe.com',
